ccad98f1c8c84954bd9897f988138dd58ffe2fd9
0xp0tato <ninadmaster05@gmail.com>
2024-10-09 13:35:12 +0530

    FIX : fixed controller routes for better navigation

--- src/main/java/com/ninadsajwan/demoSpringBootProject/controller/UserController.java
+++ src/main/java/com/ninadsajwan/demoSpringBootProject/controller/UserController.java
@@ -10,31 +10,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/")
+    @GetMapping
     public List<UserEntity> getAllUsers(){
         return userService.getAllTopics();
 
     }
 
-    @PostMapping("/")
+    @PostMapping
     public void createUser(@Valid @RequestBody CreateUserDTO user){
         userService.addUser(user);
     }
 
-    @PatchMapping("/")
-    public void updateUser(@RequestBody UpdateUserDTO user) {
+    @PatchMapping
+    public void updateUser(@RequestParam UUID id, @RequestBody UpdateUserDTO user) {
+        System.out.println("-------->" + id);
         userService.updateUser(user);
     }
 
-    @DeleteMapping("/")
-    public void deleteUser(@RequestBody DeleteUserDTO user){
+    @DeleteMapping
+    public void deleteUser(@RequestParam UUID id, @RequestBody DeleteUserDTO user){
+        System.out.println("---------->>" + id);
         userService.deleteUser(user);
     }
 }
