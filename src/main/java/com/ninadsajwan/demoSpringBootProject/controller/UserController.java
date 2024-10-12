e25321b0baf7e0acc5bf6606e395c8151762a39c
0xp0tato <ninadmaster05@gmail.com>
2024-10-12 12:42:50 +0530

    UPDATE : Added exception handler for request validation

--- src/main/java/com/ninadsajwan/demoSpringBootProject/controller/UserController.java
+++ src/main/java/com/ninadsajwan/demoSpringBootProject/controller/UserController.java
@@ -7,9 +7,14 @@ import com.ninadsajwan.demoSpringBootProject.entity.UserEntity;
 import com.ninadsajwan.demoSpringBootProject.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -37,5 +42,18 @@ public class UserController {
     @DeleteMapping
     public void deleteUser(@RequestParam UUID id) {
         userService.deleteUser(id);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
